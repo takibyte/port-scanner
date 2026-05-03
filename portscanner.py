ad7ed33b970f9df8de03aa734bda3076a9ca9394
takibyte <29111453+takibyte@users.noreply.github.com>
2026-05-03 20:10:27 +1000

    Refactor port scanning function

--- portscanner.py
+++ portscanner.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
 """
-
 A simple port scanner utilising the python socket module.
 
 """
 
 import socket
 
-def display_host():
-    hostname = socket.gethostname()
-    ip = socket.gethostbyname("localhost")
+def scan_port(host, port):
+    with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as s:
+        try:
+            s.settimeout(1)
+            result = s.connect_ex((host, port))
 
-    print(f"Hostname: {hostname}")
-    print(f"IP: {ip}")
+            return result == 0
+        
+        except socket.timeout:
+            print("Error: The scan has timed out.")
+            return False
+        
+        except socket.gaierror:
+            print("Error: The host couldn't resolve.")
+            return False
 
-display_host() 
+if scan_port("localhost", 22):
+    print("port open")
+else:
+    print("port closed")
 
 
-
-
-def scan_port():
-    s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-
-
-    return
-
-
-#with socket.socket(family=AF_INET, type=SOCK_STREAM) as s:
     
