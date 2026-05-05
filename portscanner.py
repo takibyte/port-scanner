070abc8669e59b421f09c5d1038b9a9d1d614611
takibyte <29111453+takibyte@users.noreply.github.com>
2026-05-05 17:23:30 +1000

    Implement command-line arguments for host and port range in port scanner

--- portscanner.py
+++ portscanner.py
@@ -4,19 +4,46 @@ A simple port scanner utilising the python socket module.
 
 """
 
+import argparse
 import socket
+import threading
+from concurrent.futures import ThreadPoolExecutor
 
-def scan_port_range(host, start_port, end_port):
+parser = argparse.ArgumentParser(
+    prog="portscanner",
+    description="A simple port scanner")
+
+
+def valid_port(value):
+    port = int(value)
+    if not (0 <= port <= 65535):
+        raise argparse.ArgumentTypeError(f"{value} is not a valid port, valid range is: 0-65535")
+    return port
+
+
+parser.add_argument('host', type=str, nargs='?', help='hostname or IPv4 address')
+parser.add_argument('--start', type=valid_port, default=0, help='start of the port range to be scanned')
+parser.add_argument('--end', type=valid_port, default=1001, help='end of the port range to be scanned')
+args = parser.parse_args()
+
+
+hostname = args.host
+ports = range(args.start, args.end)
+
+print_lock = threading.Lock()
+
+def scan_port(host, port):
     try:
-        for i in range(start_port, end_port + 1):
-            with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as s:
-                s.settimeout(1)
+        with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as s:
+            s.settimeout(1)
 
-                result = s.connect_ex((host, i))
+            result = s.connect_ex((host, port))
+
+            with print_lock:
                 if result == 0:
-                    print(f"Open port: {i}", end=" - ")
+                    print(f"Open port: {port}", end=" - ")
                     try:
-                        print(socket.getservbyport(i))
+                        print(socket.getservbyport(port))
                     except OSError:
                         print("Error: Service not known.")
 
@@ -29,6 +56,9 @@ def scan_port_range(host, start_port, end_port):
     except socket.gaierror:
         print("Error: The host couldn't resolve.")
         return False
+    
 
-scan_port_range("localhost", 0, 100)
+with ThreadPoolExecutor(max_workers=100) as executor:
+    executor.map(lambda port : scan_port(hostname, port), ports)
+
 
